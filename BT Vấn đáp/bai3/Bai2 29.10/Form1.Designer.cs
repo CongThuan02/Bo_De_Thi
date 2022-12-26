f414046442d1b58a6d1c576be747e2c0f145bf60
Congthuan02 <2021050033@student.humg.edu.vn>
2022-12-26 10:28:42 +0700

    1

---
+++ BT Vấn đáp/bai3/Bai2 29.10/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Bai2_29._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkHienthi = new System.Windows.Forms.CheckBox();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // chkHienthi
+            // 
+            this.chkHienthi.AutoSize = true;
+            this.chkHienthi.Location = new System.Drawing.Point(137, 254);
+            this.chkHienthi.Name = "chkHienthi";
+            this.chkHienthi.Size = new System.Drawing.Size(125, 20);
+            this.chkHienthi.TabIndex = 2;
+            this.chkHienthi.Text = "Hiển thị thông tin";
+            this.chkHienthi.UseVisualStyleBackColor = true;
+            this.chkHienthi.CheckedChanged += new System.EventHandler(this.chkHienthi_CheckedChanged);
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.Location = new System.Drawing.Point(109, 318);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(153, 40);
+            this.btnDangnhap.TabIndex = 3;
+            this.btnDangnhap.Text = "Đăng nhập";
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            this.btnDangnhap.Click += new System.EventHandler(this.btnDangnhap_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(395, 318);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(126, 40);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(215, 98);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(306, 22);
+            this.txtTen.TabIndex = 5;
+            this.txtTen.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(215, 172);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(306, 22);
+            this.txtMatKhau.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangnhap);
+            this.Controls.Add(this.chkHienthi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkHienthi;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtMatKhau;
+    }
+}
+
---
+++ BT Vấn đáp/bai3/Bai2 29.10/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bai2_29._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void chkHienthi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkHienthi.Checked == true)
+                txtMatKhau.PasswordChar = '\0';
+            else
+                txtMatKhau.PasswordChar = '*';
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            string user = "admin";
+            string pass = "123456";
+            if (user.Equals(txtTen.Text) && pass.Equals(txtMatKhau.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+                MessageBox.Show("Không đăng nhập thành công");
+        }
+    }
+}
